3054e90800628763d5e9dc3f9c054f651255c99d
Jones-Mendes <jonesoliver99@hotmail.com>
2025-04-11 12:01:24 -0300

    atividadesJS

---
+++ javascript/atividade-4.js
@@ -0,0 +1,11 @@
+function calcularTinta(largura, altura){
+    const area = largura* altura
+    const litroPorMetro = 2
+
+    const litro = area*litroPorMetro
+
+    console.log(litro)
+
+}
+
+calcularTinta(4, 3)
---
+++ javascript/atividade-5.js
@@ -0,0 +1,12 @@
+//função receberá dias alugados
+// o valor por dia é 250
+
+//multiplicar os dias de aluguel para dar o resultado
+function calcularAluguel(dias) {    
+    const valorPorDia = 250
+
+    const calculo =dias * valorPorDia
+
+    console.log(calculo)
+    
+}
