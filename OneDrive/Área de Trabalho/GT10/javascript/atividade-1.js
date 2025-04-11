6020471edf0a3d292f65c0a0d0b2f787f455f907
Jones-Mendes <jonesoliver99@hotmail.com>
2025-04-11 11:51:21 -0300

    Atividades_JS

---
+++ OneDrive/Área de Trabalho/GT10/javascript/atividade-1.js
@@ -0,0 +1,12 @@
+const nome = "Jones"
+const idade = 29
+const altura =1.75
+const maiorDeIdade = true
+console.log("Olá seu nome é " + nome + " você tem "+idade+ " anos e sua altura é "+altura)
+console.log("Olá você é maior de idade? " + maiorDeIdade) 
+
+//Template string ou string dinamica
+const frase = `Olá seu nome é ${nome}`
+console.log(frase)
+
+console.log
---
+++ OneDrive/Área de Trabalho/GT10/javascript/atividade-3.js
@@ -0,0 +1,8 @@
+function areaParede(base, altura){
+    const area= base*altura
+
+    console.log(area)
+}
+
+areaParede(4, 5)
+areaParede(3, 20)
